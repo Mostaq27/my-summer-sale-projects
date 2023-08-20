442fa5ca43689b2ed39fb0534db4dc985f388e01
Mostaq27 <mostaq99muzahid@gmail.com>
2023-08-21 01:08:09 +0600

    third commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
- let total =0;
+let total =0;
+const makePurchase = document.getElementById('btn-purchase');
+const couponBtn= document.getElementById('btn-apply');
+const couponField = document.getElementById('coupon-code');
+
+
+
+makePurchase.addEventListener('click', function(){
+  // alert('congratulations! you have purchase the product')
+})
+
+
 function handleCLikBtn(target){
    const selectedItemContainer = document.getElementById("selected-items"); 
  const productName = target.childNodes[3].childNodes[3].innerText;
@@ -18,5 +29,24 @@ function handleCLikBtn(target){
 
  total +=price;
  document.getElementById("total").innerText = total.toFixed(2);
+
+
+ if(total>=0){
+  makePurchase.removeAttribute('disabled') || couponBtn.removeAttribute('disabled')
+ }else{
+  makePurchase.setAttribute('disabled', 'true') || couponBtn.setAttribute('disabled', 'true')
+ }
  console.log(total)
+ 
+ couponBtn.addEventListener('click', function(){
+  const couponFieldValue = couponField.value;
+  if(total>200 && couponFieldValue == "SELL200"){
+    const discount= total*0.2;
+    console.log(discount);
+    document.getElementById('discount').innerText= discount;
+    const finalTotal = total- discount;
+    document.getElementById('final-total').innerText= finalTotal;
+  }
+})  
+
 }
